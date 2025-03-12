9f41000e6ff2ca474ab4f62dd7e304eaf693b643
tajpuriya27 <tajpuriyasunil27@gmail.com>
2025-03-12 10:11:39 +0545

    feat: ipify added

--- src/app/ip/page.tsx
+++ src/app/ip/page.tsx
@@ -3,7 +3,22 @@ import React, { useEffect } from "react";
 
 const Page = () => {
   const [ip, setIp] = React.useState<string>("fetching ip");
+  const [ipFromipify, setIPFromipify] = React.useState<string>(
+    "Fetching IP using ipify"
+  );
+
+  const fetchIP = async () => {
+    try {
+      const response = await fetch("https://api.ipify.org");
+      console.log("response from ipify.org", response);
+      const publicIP = await response.text();
+      setIPFromipify(publicIP);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   useEffect(() => {
+    fetchIP();
     fetch("/api/get-ip", {
       method: "POST",
     })
@@ -14,7 +29,12 @@ const Page = () => {
       });
   }, []);
 
-  return <div>Your IP is {ip}</div>;
+  return (
+    <div>
+      IP grabbed by our backend {ip}
+      <div style={{ margin: "20px" }}>IP grabbed by ipify: {ipFromipify}</div>
+    </div>
+  );
 };
 
 export default Page;
